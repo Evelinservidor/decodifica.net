d265bd0d28ce7214439629924ee719f33a56df26
Evelinservidor <oficialdrulen@gmail.com>
2026-06-10 23:35:54 +0200

    fix: exclude __trash__ from git and type check

--- __trash__/signout.ts
+++
@@ -1,8 +0,0 @@
-import type { APIRoute } from 'astro';
-import { createSupabaseServerClient } from '~/lib/supabase';
-
-export const POST: APIRoute = async ({ cookies, redirect }) => {
-  const supabase = createSupabaseServerClient(cookies);
-  await supabase.auth.signOut();
-  return redirect('/');
-};
