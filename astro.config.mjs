1ea2440b8461a7b62ac4ee71f111fc031db03833
Evelinservidor <oficialdrulen@gmail.com>
2026-06-11 01:06:59 +0200

    fix: change base path to '/' for custom domain deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://decodifica.net',
-  base: '/decodifica.net',
+  base: '/',
   output: 'static',
   integrations: [
     tailwind({
